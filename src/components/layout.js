edc0fba433c2e2fce3477aba16ec2c54d8dda56f
Hawei <hawei@paypal.com>
2020-01-31 14:14:09 +0800

    feat: add layout component.

---
+++ src/components/layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+// 文章布局， 带导航栏
+const ListLink = props => (
+    <li>
+        <Link to={props.to}>{props.children}</Link>
+    </li>
+)
+
+export default ({ children }) => (
+    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <header style={{ marginBottom: `1.5rem` }}>
+        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+          <h3 style={{ display: `inline` }}>MySweetSite</h3>
+        </Link>
+        <ul style={{ listStyle: `none`, float: `right` }}>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+    </div>
+  )
---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <h1>about</h1>
+        <span>description</span>
+    </div>
+)
