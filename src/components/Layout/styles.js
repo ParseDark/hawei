cac624aeb0e3bfb753c0ce0d9b4b40272574ea76
Hawei <hawei@paypal.com>
2020-01-31 15:46:38 +0800

    dsa

--- src/components/Layout/styles.js
+++ src/components/Layout/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { rhythm } from "../../utils/typography"
+import { rhythm } from "../../utils/typography";
 
 export const Container = styled.div`
     margin: 3rem auto;
